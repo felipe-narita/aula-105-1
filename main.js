2edf82fd2b5442fb34d5053b9b42e197a97687d4
felipe-narita <132304954+felipe-narita@users.noreply.github.com>
2024-04-01 17:42:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+//https://teachablemachine.withgoogle.com/models/IGAZy_7tE/
+
+Webcam.set({
+width:350,
+height:300,
+image_format : 'png',
+});
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach( '#camera' );
+
+function takeSnapshot()
+{
+Webcam.snap(function(data_uri)   {
+    document.getElementById("result").innerHTML = '<img id= "captured_image" src="'+data_uri+'">';
+});
+}
+
+console.log('versão ml5:', ml5.version);
+classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+img = document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+ if (error) {
+    console.error(error);
+ } else {
+    console.log(results);
+    document.getElementById("resultObjectName").innerHTML = results[0].label;
+    document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+ }    
+}
